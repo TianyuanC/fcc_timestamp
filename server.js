2038b0c656e78316a8a1e429b9fdc205f665da7f
Glitch (polite-wombat) <none>
2018-10-09 21:35:44 +0000

    👂👟 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+
+
+const cors = require('cors');
+app.use(cors({optionSuccessStatus: 200})); 
+
+app.use(express.static('public'));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get("/api/timestamp/:time?", function (req, res) {
+  let time = req.params.time;
+  
+  if (!time) {
+    time = new Date();
+  }
+  time = new Date(time);
+  
+  if (time === "Invalid Date") {
+    res.json({
+      unix: null,
+      utc: "Invalid Date"
+    })
+    return;
+  }
+  
+  res.json({
+    unix: time.getTime(),
+    utc: time.toUTCString()
+    
+  });
+});
+
+const listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
